ea7a2f085ba0f411c3ec583bddace6c14fe6904c
AdnanK1 <adnangitonga@gmail.com>
2022-05-01 01:12:46 +0300

    Tested the source.py file and it ran without fail.

--- app/test_source.py
+++ app/test_source.py
@@ -1,7 +1,13 @@
 import unittest
 from models import source
-
+Source = source.Source
 class SourceTest(unittest.TestCase):
 
-    def setUp(self) -> None:
-        return super().setUp()
+    def setUp(self):
+        self.new_source = Source('engadget','Engadget')
+
+    def test_instance(self):
+        self.assertTrue(isinstance( self.new_source,Source))
+
+if __name__ == '__main__':
+    unittest.main()
