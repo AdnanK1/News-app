af311c6d05493c2a268d6bd3cafde0493eb68d4b
AdnanK1 <adnangitonga@gmail.com>
2022-05-01 00:02:52 +0300

    Created the instance to store the api key.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from .config import DevConfig
 
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config = True)
 app.config.from_object(DevConfig)
 
 from app import routes
