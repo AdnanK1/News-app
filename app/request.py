cfde4e6ae06db7c46d3ec9eed140de7eccaa2688
AdnanK1 <adnangitonga@gmail.com>
2022-05-01 01:56:23 +0300

    Imported the base url to request file.

--- app/request.py
+++ app/request.py
@@ -1,4 +1,11 @@
 from app import app
+import urllib.request,json
+from .models import newsarticles
+
+Newsarticles = newsarticles.Newsarticles
 
 #Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+#Getting the base url
+base_url = app.config['NEWS_API_BASE_URL']
